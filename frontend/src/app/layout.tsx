f025f1b68b6403cb2630c280174a2624c9afefd0
Joshua Zhou <j.zhou11@share.epsb.ca>
2025-03-21 16:17:02 -0400

    Add tab icon image

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Joshua Zhou',
   description: 'my portfolio',
+  icons: {
+    icon: '/assets/penguin.jpg',
+  }
 }
 
 export const viewport: Viewport = {
